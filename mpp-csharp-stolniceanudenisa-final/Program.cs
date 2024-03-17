34300bdbfaf34a1c5d64abd00b05fff9eed845a4
stolniceanudenisa <stolniceanudenisa@yahoo.com>
2024-03-17 10:32:42 +0200

    logger works

---
+++ mpp-csharp-stolniceanudenisa-final/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace mpp_csharp_stolniceanudenisa_final
+{
+    internal class Program
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        
+        public static void Main(string[] args)
+        {
+
+            log.Info("Aprilie!");
+            Console.WriteLine("E bine");
+            
+            // System.Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ mpp-csharp-stolniceanudenisa-final/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("mpp_csharp_stolniceanudenisa_final")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("mpp_csharp_stolniceanudenisa_final")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("F7FF8DD5-C9A6-4C98-B0F4-9B15065DB51D")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: log4net.Config.XmlConfigurator(ConfigFile = "log4net.config")]
